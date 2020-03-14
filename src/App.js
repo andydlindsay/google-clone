3abef294cd674586453c20af1eb4227fb914be87
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-03-14 14:33:27 -0700

    filter search results based on input from SearchBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import data from './db/mock-db';
 import SearchBar from './components/SearchBar';
 import SearchList from './components/SearchList';
 
 function App(props) {
+  const [searchResults, setSearchResults] = useState(data);
+
+  const handleSubmit = (val) => {
+    const filtered = data.filter(d => d.content.includes(val))
+    setSearchResults(filtered);
+  };
+
   const msg = props.title || 'Google Clone';
   return (
     <div className="App">
       <h1>{ msg }</h1>
-      <SearchBar />
-      <SearchList data={ data } />
+      <h2>Found { searchResults.length } results!</h2>
+      <SearchBar handleSubmit={ handleSubmit } />
+      <SearchList data={ searchResults } />
     </div>
   );
 }
